7520e78f5fc0d2f916b61537029821cc57c81650
Cedric <cedric2.blaser@gmail.com>
2024-12-07 04:06:48 +0100

    fix env values.

--- kibana_user_setup.sh
+++ kibana_user_setup.sh
@@ -2,33 +2,17 @@
 set -e
 
 # Wait for Elasticsearch to be ready
-until curl -s http://elasticsearch:9200 -u elastic:$ELASTICSEARCH_PASSWORD; do
+until curl -s -u elastic:$ELASTICSEARCH_PASSWORD_MAIN http://elasticsearch:9200; do
   echo "Waiting for Elasticsearch to start..."
   sleep 5
 done
 
-# Create Kibana system role
-curl -X POST "http://elasticsearch:9200/_security/role/kibana_system" \
+# Change the password for the kibana_system user
+curl -X POST "http://elasticsearch:9200/_security/user/kibana_system/_password" \
   -H "Content-Type: application/json" \
   -u elastic:$ELASTICSEARCH_PASSWORD_MAIN \
   -d '{
-    "cluster": ["monitor"],
-    "indices": [
-      {
-        "names": [".kibana*", "kibana_sample_data*"],
-        "privileges": ["all"]
-      }
-    ]
+    "password": "'$ELASTICSEARCH_PASSWORD'"
   }'
 
-# Create Kibana system user
-curl -X POST "http://elasticsearch:9200/_security/user/kibana_system" \
-  -H "Content-Type: application/json" \
-  -u elastic:$ELASTICSEARCH_PASSWORD_MAIN \
-  -d '{
-    "password": "'$ELASTICSEARCH_PASSWORD'",
-    "roles": ["kibana_system"],
-    "full_name": "Kibana System User"
-  }'
-
-echo "Elasticsearch setup complete!"
+echo "Password for kibana_system user has been updated successfully."
