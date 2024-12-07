4552445840166642553c99ce17dff0c5e4da6b23
Cedric <cedric2.blaser@gmail.com>
2024-12-07 23:15:06 +0100

    fix get_sol_price

--- data/data_format.py
+++ data/data_format.py
@@ -5,10 +5,10 @@ from constants import PRICE_COLUMN, SOLANA_PRICE
 
 
 async def get_sol_price(r: redis.Redis) -> float:
-    # Todo add integration test
-    price = float(await r.get(SOLANA_PRICE))
+    price = await r.get(SOLANA_PRICE)
     if price is None:
-        return 150
+        return float(150)
+    return float(price)
 
 
 def transform_price_to_tokens_per_sol(data: pd.DataFrame, solana_price: float) -> pd.DataFrame:
